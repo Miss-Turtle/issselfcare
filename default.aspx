b6e6b8c668a35965779020b53a87e642d3cda4ae
Sandrine Martinez <sandrine.martinez1@gmail.com>
2018-11-13 19:51:56 +0100

    Treatment doublequote
    start treatment doublequote

---
+++ default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Inherits="Microsoft.SharePoint.Publishing.TemplateRedirectionPage,Microsoft.SharePoint.Publishing,Version=15.0.0.0,Culture=neutral,PublicKeyToken=71e9bce111e9429c" %> <%@ Reference VirtualPath="~TemplatePageUrl" %> <%@ Reference VirtualPath="~masterurl/custom.master" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<html xmlns:mso="urn:schemas-microsoft-com:office:office" xmlns:msdt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882"><head>
+<!--[if gte mso 9]><SharePoint:CTFieldRefs runat=server Prefix="mso:" FieldList="FileLeafRef,Comments,PublishingStartDate,PublishingExpirationDate,PublishingContactEmail,PublishingContactName,PublishingContactPicture,PublishingPageLayout,PublishingVariationGroupID,PublishingVariationRelationshipLinkFieldID,PublishingRollupImage,Audience,PublishingIsFurlPage,PublishingPageImage,PublishingPageContent,SummaryLinks,SummaryLinks2,SeoBrowserTitle,SeoMetaDescription,SeoKeywords,RobotsNoIndex"><xml>
+<mso:CustomDocumentProperties>
+<mso:PublishingPageContent msdt:dt="string"></mso:PublishingPageContent>
+<mso:ContentType msdt:dt="string">Welcome Page</mso:ContentType>
+<mso:PublishingPageLayout msdt:dt="string">/_catalogs/masterpage/BlankWebPartPage.aspx, Blank Web Part page</mso:PublishingPageLayout>
+</mso:CustomDocumentProperties>
+</xml></SharePoint:CTFieldRefs><![endif]-->
+<title>Home</title></head>
--- search-page-js/issselfcareSearch.js
+++ search-page-js/issselfcareSearch.js
@@ -1,37 +1,73 @@
 // Wrapping jQuery variable to prevent conflict with other libraries
-(function ($) {      
+(function ($) {
+    // Get parameters
+    var getUrlParameters = window.location.href.split("#");
+    var getUrlBasePart = getUrlParameters[0];
+    var getUrlParametersPart = getUrlParameters[1];
+    console.log("getUrlParametersPart");
+    console.log(getUrlParametersPart);
+    // Verify if parameters exists in url and
+    // Determine if there are filter on url qurey search
+    // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
+    if (getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=" >= 0)) {
+        // Verify if doublequotes are in the url query search        
+        if (getUrlParametersPart.indexOf("\"") >= 0) {
+            console.log("doublequotes");
+            console.log(getUrlParametersPart.indexOf("\""));
+            console.log(getUrlParametersPart.indexOf("\"", getUrlParametersPart.indexOf("\"") + 1));
+            var firstBlockquotePosition = getUrlParametersPart.indexOf("\"");
+            var secondBlockquotePosition = getUrlParametersPart.indexOf("\"", firstBlockquotePosition + 1);
+            (firstBlockquotePosition >= 0 && secondBlockquotePosition >= 0) ? citationTreatment() : console.log("1 blockquote");
+            function citationTreatment(){
+                console.log("2 blockquote");                
+                var citation = getUrlParametersPart.slice(firstBlockquotePosition, secondBlockquotePosition + 1);
+                console.log(citation);
+            }
+        }
+        else {
+            // Verify if spaces are in the url query search
+            if (getUrlParametersPart.indexOf("%20") >= 0 || getUrlParametersPart.indexOf(" ") >= 0) {
+                // Replace spaces by + on the url query search and reload page
+                var newUrlParameters = "";
+                if (getUrlParametersPart.indexOf("%20") >= 0) {
+                    newUrlParameters = getUrlParametersPart.replace("%20", "%2B");
+                }
+                if (getUrlParametersPart.indexOf(" ") >= 0) {
+                    newUrlParameters = getUrlParametersPart.replace(" ", "%2B");
+                }
+                window.location = getUrlBasePart + "#" + newUrlParameters;
+                window.location.reload(true);
+            }
+        }
+    }
     // Wait all document's chargement
     $(document).ready(function () {
-        // Get parameters
-        var getUrlParameters = window.location.href.split("#");
-        var getUrlBasePart = getUrlParameters[0];
-        var getUrlParametersPart = getUrlParameters[1];
         // Verify if parameters exists in url and
         // Get url to determine if there are filter on url qurey search
         // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
-        if(getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=") >= 0){            
+        if (getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=") >= 0) {
             // Change input value to hide %2B by space " " on field view
             var newInputSearchText = getUrlParametersPart.replace("k=", "");
-            if (newInputSearchText.indexOf("%2B") >= 0) { 
-                newInputSearchText = newInputSearchText.replace("%2B", " "); 
-                $("input[title=\"Search...\"]").val(newInputSearchText);  
-            }            
+            if (newInputSearchText.indexOf("%2B") >= 0) {
+                newInputSearchText = newInputSearchText.replace("%2B", " ");
+                $("input[title=\"Search...\"]").val(newInputSearchText);
+            }
             // Verify if the star item is on search parameters url
-            if(getUrlParametersPart.indexOf("k=*") >= 0){
+            if (getUrlParametersPart.indexOf("k=*") >= 0) {
                 // Hide the start item in the input field
                 $("input[title=\"Search...\"]").val("");
-            }         
-        }    
+            }
+        }
         // On click reload page with new search
         $("a[title=\"Search\"]").on("click", function (event) {
             getUrlParametersForRedefineSearch(window.location.href, this);
         });
         // On key press enter reload page with new search
-        $("input[title=\"Search...\"]").keypress(function(event){
-            if(event.which == 13){
+        $("input[title=\"Search...\"]").keypress(function (event) {
+            if (event.which == 13) {
                 getUrlParametersForRedefineSearch(window.location.href, this);
             }
-        });    
+        });
 
         // Variables to calculate banner dimension and keep proportionality
         var widthBanner = 1304;
@@ -55,7 +91,7 @@
     });
 
     // Get window location href to redefine search query with + and relaod page with new url
-    function getUrlParametersForRedefineSearch(windowLocationHref, element){
+    function getUrlParametersForRedefineSearch(windowLocationHref, element) {
         var getUrlParametersRedefine = windowLocationHref.split("#");
         var getUrlBasePartRedefine = getUrlParametersRedefine[0];
         var getUrlParametersPartRedefine = getUrlParametersRedefine[1];
@@ -63,11 +99,11 @@
         // Verify if parameters exists in url and
         // Get url to determine if there are filter on url qurey search
         // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
-        if(getUrlParametersRedefine.length > 1 && getUrlParametersPartRedefine.indexOf("k=") >= 0 && getUrlParametersPartRedefine.indexOf("%20") >= 0) {
+        if (getUrlParametersRedefine.length > 1 && getUrlParametersPartRedefine.indexOf("k=") >= 0 && getUrlParametersPartRedefine.indexOf("%20") >= 0) {
             newSearchItem = newSearchItem.replace("k=", "");
             newSearchItem = newSearchItem.replace("%20", "+");
-            window.location.href = getUrlBasePartRedefine + "#" + newSearchItem;     
-            $getClientControl(element).search(newSearchItem);   
+            window.location.href = getUrlBasePartRedefine + "#" + newSearchItem;
+            $getClientControl(element).search(newSearchItem);
         }
     }
 
