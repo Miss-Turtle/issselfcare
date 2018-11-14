f0282967e0b7d52fe35d138fe859ec16642b3c64
Sandrine Martinez <sandrine.martinez1@gmail.com>
2018-11-14 23:36:19 +0100

    Adjustement search script doublequote and masterpage header
    treatment doublequote query and chargement of the script if is page search/issselfcare and load script in header masterpage

--- search-page-js/issselfcareSearch.js
+++ search-page-js/issselfcareSearch.js
@@ -4,94 +4,69 @@
     var getUrlParameters = window.location.href.split("#");
     var getUrlBasePart = getUrlParameters[0];
     var getUrlParametersPart = getUrlParameters[1];
-    console.log("getUrlParametersPart");
-    console.log(getUrlParametersPart);
-    // Verify if parameters exists in url and
-    // Determine if there are filter on url qurey search
-    // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
-    if (getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=" >= 0)) {
-        // Verify if doublequotes are in the url query search        
-        if (getUrlParametersPart.indexOf("\"") >= 0) {
-            console.log("doublequotes");
-            console.log(getUrlParametersPart.indexOf("\""));
-            console.log(getUrlParametersPart.indexOf("\"", getUrlParametersPart.indexOf("\"") + 1));
-            var firstBlockquotePosition = getUrlParametersPart.indexOf("\"");
-            var secondBlockquotePosition = getUrlParametersPart.indexOf("\"", firstBlockquotePosition + 1);
-            (firstBlockquotePosition >= 0 && secondBlockquotePosition >= 0) ? citationTreatment() : console.log("1 blockquote");
-            function citationTreatment(){
-                console.log("2 blockquote");                
-                var citation = getUrlParametersPart.slice(firstBlockquotePosition, secondBlockquotePosition + 1);
-                console.log(citation);
-            }
-        }
-        else {
-            // Verify if spaces are in the url query search
-            if (getUrlParametersPart.indexOf("%20") >= 0 || getUrlParametersPart.indexOf(" ") >= 0) {
-                // Replace spaces by + on the url query search and reload page
-                var newUrlParameters = "";
-                if (getUrlParametersPart.indexOf("%20") >= 0) {
-                    newUrlParameters = getUrlParametersPart.replace("%20", "%2B");
+    if (getUrlBasePart.indexOf("/Search/ISSSelfcare.aspx") >= 0) { 
+        // Wait all document's chargement
+        $(document).ready(function () {
+            // Verify if parameters exists in url and
+            // Get url to determine if there are filter on url qurey search
+            // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
+            if (getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=") >= 0) {
+                // Change input value to hide %2B by space " " on field view
+                var newInputSearchText = getUrlParametersPart.replace("k=", "");
+                var searchAddUrlEncodeTable = newInputSearchText.match(/%2B/gi);
+                if (searchAddUrlEncodeTable != null) {
+                    newInputSearchText = newInputSearchText.replace(/%2B/gi, " ");
+                    $("input[title=\"Search...\"]").val(newInputSearchText);
                 }
-                if (getUrlParametersPart.indexOf(" ") >= 0) {
-                    newUrlParameters = getUrlParametersPart.replace(" ", "%2B");
+                var searchSpaceUrlEncodeTable = newInputSearchText.match(/%20/gi);
+                if (searchSpaceUrlEncodeTable != null) {
+                    newInputSearchText = newInputSearchText.replace(/%20/gi, " ");
+                    $("input[title=\"Search...\"]").val(newInputSearchText);
                 }
-                window.location = getUrlBasePart + "#" + newUrlParameters;
-                window.location.reload(true);
+                var searchDoubleQuoteUrlEncodeTable = newInputSearchText.match(/%22/gi);
+                if (searchDoubleQuoteUrlEncodeTable != null) {
+                    newInputSearchText = newInputSearchText.replace(/%22/gi, "\"");
+                    $("input[title=\"Search...\"]").val(newInputSearchText);
+                }
             }
-        }
-    }
-    // Wait all document's chargement
-    $(document).ready(function () {
-        // Verify if parameters exists in url and
-        // Get url to determine if there are filter on url qurey search
-        // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
-        if (getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=") >= 0) {
-            // Change input value to hide %2B by space " " on field view
-            var newInputSearchText = getUrlParametersPart.replace("k=", "");
-            if (newInputSearchText.indexOf("%2B") >= 0) {
-                newInputSearchText = newInputSearchText.replace("%2B", " ");
-                $("input[title=\"Search...\"]").val(newInputSearchText);
-            }
-            // Verify if the star item is on search parameters url
-            if (getUrlParametersPart.indexOf("k=*") >= 0) {
-                // Hide the start item in the input field
-                $("input[title=\"Search...\"]").val("");
-            }
-        }
-        // On click reload page with new search
-        $("a[title=\"Search\"]").on("click", function (event) {
-            getUrlParametersForRedefineSearch(window.location.href, this);
-        });
-        // On key press enter reload page with new search
-        $("input[title=\"Search...\"]").keypress(function (event) {
-            if (event.which == 13) {
+            // On click reload page with new search
+            $("a[title=\"Search\"]").on("click", function (event) {
+                console.log("click");
                 getUrlParametersForRedefineSearch(window.location.href, this);
-            }
-        });
+            });
+            // On key press enter reload page with new search
+            $("input[title=\"Search...\"]").keypress(function (event) {
+                if (event.which == 13) {
+                    console.log("enter");
+                    getUrlParametersForRedefineSearch(window.location.href, this);
+                }
+            });
 
-        // Variables to calculate banner dimension and keep proportionality
-        var widthBanner = 1304;
-        var heightBanner = 140;
-        var widthContentWrapper = $('.contentwrapper').width();
-        var pourcentHeightBanner = (widthContentWrapper * 100) / widthBanner;
-        var newHeightBanner = (pourcentHeightBanner * heightBanner) / 100;
+            // Variables to calculate banner dimension and keep proportionality
+            var widthBanner = 1304;
+            var heightBanner = 140;
+            var widthContentWrapper = $('.contentwrapper').width();
+            var pourcentHeightBanner = (widthContentWrapper * 100) / widthBanner;
+            var newHeightBanner = (pourcentHeightBanner * heightBanner) / 100;
 
-        // Add a div in the top content wrapper for add banner
-        $('.contentwrapper').prepend("<div id='bannerSearch'></div>");
-        // Add height for the div bannerSearch
-        $('.contentwrapper #bannerSearch').css({
-            "height": (Math.round(newHeightBanner) - 4) + "px"
-        });
+            // Add a div in the top content wrapper for add banner
+            $('.contentwrapper').prepend("<div id='bannerSearch'></div>");
+            // Add height for the div bannerSearch
+            $('.contentwrapper #bannerSearch').css({
+                "height": (Math.round(newHeightBanner) - 4) + "px"
+            });
 
-        // Load all necessary SharePoint JavaScript libraries
-        SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
-            // Get url of the plateform
-            loadListIssselfcareUrlPlateforme('issselfcareUrlPlateforme');
+            // Load all necessary SharePoint JavaScript libraries
+            SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+                // Get url of the plateform
+                loadListIssselfcareUrlPlateforme('issselfcareUrlPlateforme');
+            });
         });
-    });
+    }
 
     // Get window location href to redefine search query with + and relaod page with new url
     function getUrlParametersForRedefineSearch(windowLocationHref, element) {
+        console.log("redefine search");
         var getUrlParametersRedefine = windowLocationHref.split("#");
         var getUrlBasePartRedefine = getUrlParametersRedefine[0];
         var getUrlParametersPartRedefine = getUrlParametersRedefine[1];
@@ -99,12 +74,76 @@
         // Verify if parameters exists in url and
         // Get url to determine if there are filter on url qurey search
         // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
-        if (getUrlParametersRedefine.length > 1 && getUrlParametersPartRedefine.indexOf("k=") >= 0 && getUrlParametersPartRedefine.indexOf("%20") >= 0) {
+        if (getUrlParametersRedefine.length > 1 && getUrlParametersPartRedefine.indexOf("k=") >= 0) {
             newSearchItem = newSearchItem.replace("k=", "");
-            newSearchItem = newSearchItem.replace("%20", "+");
-            window.location.href = getUrlBasePartRedefine + "#" + newSearchItem;
-            $getClientControl(element).search(newSearchItem);
+            /*if(getUrlParametersPartRedefine.indexOf("%22") >= 0){
+                newSearchItem = newSearchItem.replace(/%22/gi, "\"");
+                window.location.href = getUrlBasePartRedefine + "#" + newSearchItem;
+            }
+            if(getUrlParametersPartRedefine.indexOf("%20") >= 0){
+                newSearchItem = newSearchItem.replace(/%20/gi, "+");
+                window.location.href = getUrlBasePartRedefine + "#" + newSearchItem;                
+            }    */
+            /*$getClientControl(element).search(newSearchItem);*/
+            citationTreatmentNoWindowReload(element, getUrlBasePartRedefine, newSearchItem);
         }
+    }
+
+    function citationTreatmentNoWindowReload(element, getUrlBasePart, newSearchItem) {
+        // Verify if doublequotes are in the url query search        
+        if (newSearchItem.indexOf("%22") >= 0) {
+            console.log("citation no treatment");
+            console.log(getUrlBasePart);
+            console.log(newSearchItem);
+            var firstDoubleQuotePosition = newSearchItem.indexOf("%22");
+            var secondDoubleQuotePosition = newSearchItem.indexOf("%22", firstDoubleQuotePosition + 1);
+            var citation = newSearchItem.slice(firstDoubleQuotePosition, secondDoubleQuotePosition + 3);
+            console.log("citation");
+            console.log(citation);
+            console.log("newSearchItem.length");
+            console.log(newSearchItem.length);
+            console.log("citation.length");
+            console.log(citation.length);
+            console.log("newSearchItem 0");
+            console.log(newSearchItem);
+            if (newSearchItem.length > citation.length) {
+                console.log("newSearchItem.length > citation.length");
+                var getUrlParametersPartCitationLess = newSearchItem.split(citation);
+                var getUrlParametersPartCitationLessFirstPart = getUrlParametersPartCitationLess[0];
+                var getUrlParametersPartCitationLessSecondPart = getUrlParametersPartCitationLess[1];
+                console.log("getUrlParametersPartCitationLess");
+                console.log(getUrlParametersPartCitationLess);
+                console.log(getUrlParametersPartCitationLessFirstPart);
+                console.log(getUrlParametersPartCitationLessFirstPart);
+                console.log(getUrlParametersPartCitationLessSecondPart);
+                citation = citation.replace(/%22/gi, "\"");
+                citation = citation.replace(/%20/gi, " ");
+                (getUrlParametersPartCitationLessFirstPart.length > 0) ?
+                    getUrlParametersPartCitationLessFirstPart = getUrlParametersPartCitationLessFirstPart.replace(/%20/gi, "+") :
+                    getUrlParametersPartCitationLessSecondPart = getUrlParametersPartCitationLessSecondPart.replace(/%20/gi, "+");
+                newSearchItem = (firstDoubleQuotePosition > 0) ? getUrlParametersPartCitationLessFirstPart + citation : citation + getUrlParametersPartCitationLessSecondPart;
+            }
+            console.log("newSearchItem 1");
+            console.log(newSearchItem);
+        }
+        else {
+            console.log("parametersReconstitution");
+            console.log(newSearchItem);
+            newSearchItem = newSearchItem.replace(/%22/gi, "\"");
+            console.log("parametersReconstitution %22");
+            console.log(newSearchItem);
+            newSearchItem = newSearchItem.replace(/%20/gi, "+");
+            console.log("parametersReconstitution %20");
+            console.log(newSearchItem);
+            newSearchItem = newSearchItem.replace(/%2B/gi, "+");
+            console.log("parametersReconstitution %2B");
+            console.log("newSearchItem 2");
+            console.log(newSearchItem);
+        }
+        console.log("newSearchItem 3");
+        console.log(newSearchItem);
+        window.location.href = getUrlBasePart + "#" + newSearchItem;
+        $getClientControl(element).search(newSearchItem);
     }
 
     // Function to load list items and get url from list issselfcareUrlPlateforme
--- search-page-js/issselfcareSearchLoad.js
+++ search-page-js/issselfcareSearchLoad.js
@@ -1,25 +1,60 @@
-// Wrapping jQuery variable to prevent conflict with other libraries
-(function ($) {
-    // Get parameters
-    var getUrlParameters = window.location.href.split("#");
-    var getUrlBasePart = getUrlParameters[0];
-    var getUrlParametersPart = getUrlParameters[1];
+// Get parameters
+var getUrlParameters = window.location.href.split("#");
+var getUrlBasePart = getUrlParameters[0];
+var getUrlParametersPart = getUrlParameters[1];
+if (getUrlBasePart.indexOf("/Search/ISSSelfcare.aspx") >= 0) {
     // Verify if parameters exists in url and
     // Determine if there are filter on url qurey search
     // [url parameters start with "k=" when no filter exist and start by "default=" when filter exist]
-    if(getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=" >= 0)){
-        // Verify if doublequotes are in the url query search
-        console.log(getUrlParametersPart.indexOf("%2522"));
-        if (getUrlParametersPart.indexOf("%2522") >= 0){
-            console.log("doublequotes");
+    if (getUrlParameters.length > 1 && getUrlParametersPart.indexOf("k=" >= 0)) {
+        // Verify if doublequotes are in the url query search        
+        if (getUrlParametersPart.indexOf("%22") >= 0 || getUrlParametersPart.indexOf("\"") >= 0) {
+            var isDoublequoteUrlEncode = false;
+            (getUrlParametersPart.indexOf("%22") >= 0) ? isDoublequoteUrlEncode = true : isDoublequoteUrlEncode = false;
+            if (!isDoublequoteUrlEncode) {
+                getUrlParametersPart = getUrlParametersPart.replace(/"/gi, "%22");
+                var firstDoubleQuotePosition = getUrlParametersPart.indexOf("%22");
+                var secondDoubleQuotePosition = getUrlParametersPart.indexOf("%22", firstDoubleQuotePosition + 1);
+                if (firstDoubleQuotePosition >= 0 && secondDoubleQuotePosition >= 0) {
+                    citationTreatmentWindowReload(getUrlBasePart, getUrlParametersPart);
+                }
+            }
         }
-        
-        // Verify if spaces are in the url query search
-        if (getUrlParametersPart.indexOf("%20") >= 0) {
-            // Replace spaces by + on the url query search and reload page
-            var newUrlParameters = getUrlParametersPart.replace("%20", "%2B");
-            window.location = getUrlBasePart + "#" + newUrlParameters;
-            window.location.reload(true);
-        }        
+        else {
+            // Verify if spaces are in the url query search            
+            if (getUrlParametersPart.indexOf("%20") >= 0 || getUrlParametersPart.indexOf(" ") >= 0) {
+                // Replace spaces by + on the url query search and reload page
+                var newUrlParameters = "";
+                var isSpaceUrlEncode = false;
+                (getUrlParametersPart.indexOf("%20") >= 0) ? isSpaceUrlEncode = true : isSpaceUrlEncode = false;
+                (isSpaceUrlEncode) ? newUrlParameters = getUrlParametersPart.replace(/%20/gi, "%2B") : newUrlParameters = getUrlParametersPart.replace(/ /gi, "%2B");
+                window.location = getUrlBasePart + "#" + newUrlParameters;
+                window.location.reload(true);
+            }
+        }
     }
-})(jQuery);
+}
+
+// Treat citation terms for Window reload
+function citationTreatmentWindowReload(getUrlBasePart, getUrlParametersPart) {
+    getUrlParametersPart = getUrlParametersPart.replace("k=", "");
+    getUrlParametersPart = getUrlParametersPart.replace(/ /gi, "%20");
+    var firstDoubleQuotePosition = getUrlParametersPart.indexOf("%22");
+    var secondDoubleQuotePosition = getUrlParametersPart.indexOf("%22", firstDoubleQuotePosition + 1);
+    var citation = getUrlParametersPart.slice(firstDoubleQuotePosition, secondDoubleQuotePosition + 3);
+    if (getUrlParametersPart.length > citation.length) {
+        var getUrlParametersPartCitationLess = getUrlParametersPart.split(citation);
+        var getUrlParametersPartCitationLessFirstPart = getUrlParametersPartCitationLess[0];
+        var getUrlParametersPartCitationLessSecondPart = getUrlParametersPartCitationLess[1];
+        (getUrlParametersPartCitationLessFirstPart.length > 0) ?
+            getUrlParametersPartCitationLessFirstPart = getUrlParametersPartCitationLessFirstPart.replace(/%20/gi, "%2B") :
+            getUrlParametersPartCitationLessSecondPart = getUrlParametersPartCitationLessSecondPart.replace(/%20/gi, "%2B");
+        var parametersReconstitution = (firstDoubleQuotePosition > 0) ? getUrlParametersPartCitationLessFirstPart + citation : citation + getUrlParametersPartCitationLessSecondPart;
+        window.location = getUrlBasePart + "#k=" + parametersReconstitution;
+        window.location.reload(true);
+    }
+    else {
+        window.location = getUrlBasePart + "#k=" + getUrlParametersPart;
+        window.location.reload(true);
+    }
+}
